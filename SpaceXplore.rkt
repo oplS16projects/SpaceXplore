a2e2e68df570e346ce4971485fb68a9ba002837f
robdoesweb <rob@robdoesweb.com>
2016-04-07 16:33:03 -0400

    Added basic entity definition and package requirements

--- SpaceXplore.rkt
+++ SpaceXplore.rkt
@@ -1,6 +1,27 @@
 #lang racket
 
+(require 2htdp/universe 2htdp/image)
+
 ;; THIS IS OUR STARTING CODE FOR THE GROUP PROJECT
 ;; ROB & LOKESH
 
 (define (game start) 'foo)
+
+(define default-sprite 'someimagehere)
+
+(define (entity x y)
+  (define xpos x)
+  (define ypos y)
+  (define sprite default-sprite)
+  (define (change-sprite new-sprite)
+    (set! sprite new-sprite))
+
+  (define (dispatch m)
+    (cond ((eq? m 'x) xpos)
+          ((eq? m 'y) ypos)
+          ((eq? m 'change-sprite) change-sprite)
+          (else "ERROR ENTITY DISPATCH")
+          ))
+  dispatch  
+ )
+
